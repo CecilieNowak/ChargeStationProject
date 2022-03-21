cdc0aeba0ea58779a50e33c7aac4090b69f2442b
camil <camil@DESKTOP-8M3PEC4>
2022-03-21 14:09:07 +0100

    oprettelse af test af displayStatus klasse. ikkr kontrolleret

--- ChargeStationProject.Test/UnitTest1.cs
+++ ChargeStationProject.Test/UnitTest1.cs
@@ -5,10 +5,12 @@ namespace ChargeStationProject.Test
     public class Tests
     {
         private IDisplay _display;
+        private IDisplay _displayS;
         [SetUp]
         public void Setup()
         {
             _display = new DisplayInstructions();
+            _displayS = new DisplayStatus();
         }
 
         [Test]
@@ -17,11 +19,18 @@ namespace ChargeStationProject.Test
             Assert.Pass();
         }
 
-        [TestCase("hej")]
+        [TestCase("Instruktioner")]
         public void DisplayInstruction_showMessage_resiveIsCorrect(string input)
         {
             _display.showMessage(input);
             Assert.That(_display.SaveMessage, Is.EqualTo(input));
+
+        }
+        [TestCase("Status")]
+        public void DisplayStatus_showMessage_resiveIsCorrect(string input)
+        {
+            _displayS.showMessage(input);
+            Assert.That(_displayS.SaveMessage, Is.EqualTo(input));
 
         }
     }
