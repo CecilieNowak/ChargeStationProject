5dff516e1066c40620a90886b64751f5a175ef78
Kamilla Stauenberg L Halkjær <au681524@uni.au.dk>
2022-03-24 14:08:58 +0100

    Test til RfId reader

--- ChargeStationClassLibrary/IRfidReader.cs
+++ ChargeStationClassLibrary/IRfidReader.cs
@@ -4,12 +4,7 @@ using System.Text;
 
 namespace ChargeStationProject
 {
-    //Klasse
-    public class RfidDetectedEventArgs : EventArgs
-    {
-        public int Rfid { get; set; }
-    }
-
+ 
     //Interface
     public interface IRfidReader
     {
